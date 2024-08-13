1115acc8fe3d5d7a9e25f95470ce4f3d5a833619
Ronald Krist <ronald.krist@cesnet.cz>
2024-08-13 16:32:32 +0200

    community default workflow input synchronized with record xor data pattern

--- oarepo_communities/workflow.py
+++ oarepo_communities/workflow.py
@@ -4,14 +4,17 @@ from oarepo_communities.utils import community_id_from_record
 
 
 def community_default_workflow(**kwargs):
-    if "record" not in kwargs and "community_id" not in kwargs:
+    if "record" not in kwargs and "data" not in kwargs:
         return None
-    if "community_id" not in kwargs:
+    if "record" in kwargs:
         community_id = community_id_from_record(kwargs["record"])
         if not community_id:
             return None
     else:
-        community_id = kwargs["community_id"]
+        try:
+            community_id = kwargs["data"]["parent"]["communities"]["default"]
+        except KeyError:
+            return None
 
     community = Community.get_record(community_id)
     return community.custom_fields.get("workflow")
