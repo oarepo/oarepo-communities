0cda367271fcc9965eacaca199ebac71b7eccfe0
ducica <dusanstojanovic256@gmail.com>
2024-09-09 10:13:08 +0200

    added communities skeleton

--- oarepo_communities/ui/communities_components/semantic-ui/templates/overridableRegistry/oarepo_communities/mapping.js
+++ oarepo_communities/ui/communities_components/semantic-ui/templates/overridableRegistry/oarepo_communities/mapping.js
@@ -1,5 +1,10 @@
+import React from "react";
+import { CommunityInvitationModal } from "@js/oarepo_communities/search/CommunityInvitationModal";
+
 const mapping = {
   "InvenioCommunities.CommunityProfileForm.GridRow.DangerZone": () => null,
+  "InvenioCommunities.CommunityMembers.InvitationsModal":
+    CommunityInvitationModal,
 };
 
 export default mapping;
