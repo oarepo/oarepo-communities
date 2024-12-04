4621c551249265ab7fa34b1b6c9ea60872c4737f
ducica <dusanstojanovic256@gmail.com>
2024-12-04 11:38:59 +0100

    checking permission for new record btn

--- oarepo_communities/ui/oarepo_communities/components.py
+++ oarepo_communities/ui/oarepo_communities/components.py
@@ -8,13 +8,31 @@ class GetCommunityComponent(UIResourceComponent):
         self, *, search_options, extra_context, identity, view_args, **kwargs
     ):
         community = view_args.get("community")
+        community_id = str(community.id)
+
+        workflow = community["custom_fields"].get("workflow", "default")
+        from oarepo_workflows.errors import InvalidWorkflowError
+        from oarepo_workflows.proxies import current_oarepo_workflows
+
+        if workflow not in current_oarepo_workflows.record_workflows:
+            raise InvalidWorkflowError(
+                f"Workflow {workflow} does not exist in the configuration."
+            )
+
+        wf = current_oarepo_workflows.record_workflows[workflow]
+        permissions = wf.permissions(
+            "create", data={"parent": {"communities": {"default": community_id}}}
+        )
+        can_create_record = permissions.allows(identity)
+
         # for consistency with invenio-communities routes
         # needed to check if there is something in the curation policy and
         # about page, so that those tabs would render in the menu
         request.community = community.to_dict()
         permissions = community.has_permissions_to(HEADER_PERMISSIONS)
+        permissions["can_create_record"] = can_create_record
         extra_context["community"] = community
         extra_context["permissions"] = permissions
-        search_options["overrides"][
-            "ui_endpoint"
-        ] = f"/communities/{community.id}/records"
+        search_options["overrides"]["ui_endpoint"] = (
+            f"/communities/{community_id}/records"
+        )
