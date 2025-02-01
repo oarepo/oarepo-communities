6df66614e717be522c7b8ded018364a452fc5a41
ducica <dusanstojanovic256@gmail.com>
2025-02-01 21:54:23 +0100

    error and loader for selectedcommunity

--- oarepo_communities/ui/communities_components/semantic-ui/templates/custom_fields/SelectedTargetCommunity.js
+++ oarepo_communities/ui/communities_components/semantic-ui/templates/custom_fields/SelectedTargetCommunity.js
@@ -3,20 +3,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { httpApplicationJson } from "@js/oarepo_ui";
 import { CommunityItem } from "@js/communities_components/CommunitySelector/CommunityItem";
-import { List } from "semantic-ui-react";
+import { List, Loader, Dimmer, Message } from "semantic-ui-react";
 import { useFormikContext, getIn } from "formik";
+import { i18next } from "@translations/oarepo_communities";
 
 const SelectedTargetCommunity = ({ fieldPath, readOnlyLabel }) => {
   const { values } = useFormikContext();
   const selectedCommunityId = getIn(values, fieldPath, "");
 
-  const { data, isLoading } = useQuery(
+  const { data, isFetching, isError } = useQuery(
     ["targetCommunity", selectedCommunityId],
     () => httpApplicationJson.get(`/api/communities/${selectedCommunityId}`),
     {
       enabled: !!selectedCommunityId,
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
       select: (data) => data.data,
     }
   );
@@ -24,19 +23,32 @@ const SelectedTargetCommunity = ({ fieldPath, readOnlyLabel }) => {
   return (
     <React.Fragment>
       <strong>{readOnlyLabel}</strong>
-      {!isLoading && (
+      {isFetching ? (
+        <Dimmer inverted active={isFetching}>
+          <Loader active />
+        </Dimmer>
+      ) : (
         <List>
-          <CommunityItem
-            community={{
-              id: data.id,
-              title: data.metadata?.title,
-              website: data.metadata?.website,
-              logo: data.links?.logo,
-              organizations: data.metadata?.organizations,
-              links: data.links,
-            }}
-          />
+          {data && (
+            <CommunityItem
+              community={{
+                id: data?.id,
+                title: data?.metadata?.title,
+                website: data?.metadata?.website,
+                logo: data?.links?.logo,
+                organizations: data?.metadata?.organizations,
+                links: data?.links,
+              }}
+            />
+          )}
         </List>
+      )}
+      {isError && (
+        <Message negative>
+          <Message.Content>
+            {i18next.t("Could not fetch community.")}
+          </Message.Content>
+        </Message>
       )}
     </React.Fragment>
   );
