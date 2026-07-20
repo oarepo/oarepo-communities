797303ff0a2d6bebc16181f05d0a7761900e87dc
ron <kristron6@gmail.com>
2026-07-20 15:36:23 +0200

    reverted deleted emails; test fix

--- oarepo_communities/records/api.py
+++ oarepo_communities/records/api.py
@@ -14,6 +14,11 @@ import dataclasses
 import logging
 from typing import TYPE_CHECKING, Any
 
+from invenio_access.models import Role, User
+from invenio_communities.members.records.models import MemberModel
+from invenio_db import db
+from oarepo_requests.notifications.generators.recipients import _extract_user_email_data
+
 log = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
@@ -43,3 +48,33 @@ class CommunityRoleRecord:
     def id(self) -> str:
         """Return the ID of the community role."""
         return f"{self.community.id}:{self.role}"
+
+    # TODO: necessary?
+    @property
+    def emails(self) -> list[str]:
+        """Return the emails of the community members."""
+        member_emails = []
+        members: list[MemberModel] = (
+            db.session.query(MemberModel)
+            .filter_by(
+                community_id=self.community.id,
+                role=self.role,
+                active=True,
+            )
+            .all()
+        )
+        for member in members:
+            try:
+                if member.user_id:
+                    user = User.query.get(member.user_id)
+                    member_emails.append(_extract_user_email_data(user))
+                if member.group_id:
+                    group = Role.query.get(member.group_id)
+                    member_emails.extend(_extract_user_email_data(user) for user in group.users)
+            except Exception:
+                log.exception(
+                    "Error retrieving user %s, group %s for community members",
+                    member.user_id,
+                    member.group_id,
+                )
+        return member_emails
