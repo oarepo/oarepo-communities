b5cf59f2cdb92e660794f6568fb7f86094f82609
ron <kristron6@gmail.com>
2026-08-13 12:19:02 +0200

    reusing CommunityMembersRecipient for CommunityRoleEmailRecipient; new tests; rebase

--- tests/test_communities/test_notifications.py
+++ tests/test_communities/test_notifications.py
@@ -9,9 +9,7 @@
 from __future__ import annotations
 
 import pytest
-from invenio_access.models import User
 from invenio_access.permissions import system_identity
-from invenio_communities.members.records.models import MemberModel
 from invenio_communities.proxies import current_communities
 from invenio_requests.customizations.event_types import CommentEventType
 
@@ -208,50 +206,6 @@ def test_locales_multiple_recipients(
         assert len(sent_mail_cz) == len(sent_mail_en) == 1
         assert sent_mail_cz[0].subject == "Žádost o publikování záznamu blabla"
         assert sent_mail_en[0].subject == "Request to publish record blabla"
-
-
-def test_notifications_not_sent_to_inactive_users(
-    db,
-    app,
-    community,
-    community_owner,
-    users,
-    logged_client,
-    draft_with_community_factory,
-    submit_request_on_draft,
-    link2testclient,
-    invite,
-    search_clear,
-):
-    """Test notification being built on review submit."""
-    mail = app.extensions.get("mail")
-    assert mail
-
-    invite(users[0], str(community.id), "reader")
-    invite(users[1], str(community.id), "curator")
-    invite(users[2], str(community.id), "curator")
-
-    member = MemberModel.query.filter_by(user_id=users[2].user.id, community_id=str(community.id)).one()
-    member.active = False
-    db.session.commit()
-
-    us = User.query.join(MemberModel).filter(MemberModel.community_id == str(community.id)).all()
-    active_users = (
-        User.query.join(MemberModel)
-        .filter((MemberModel.community_id == str(community.id)) & (MemberModel.active.is_(True)))
-        .all()
-    )
-
-    assert users[2].user in us
-    assert users[2].user not in active_users
-
-    creator = users[0]
-    draft1 = draft_with_community_factory(creator.identity, str(community.id), custom_workflow="curator_publish")
-    with mail.record_messages() as outbox:
-        submit_request_on_draft(creator.identity, draft1["id"], "publish_draft")
-        # check notification is build on submit
-        assert len(outbox) == 1  # only one curator active now
-        assert outbox[0].send_to == {users[1].email}
 
 
 def test_comment_notifications(
